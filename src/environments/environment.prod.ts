9f57f4e0b87840f00e4079e844dfc27e00c8d27c
castillo0224 <castillo0224@gmail.com>
2019-09-01 00:21:13 -0500

    deploy firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDLFs5k509EqaFIx3hDGFio55vivvt-HQs",
+    authDomain: "app-crud-client-firebase.firebaseapp.com",
+    databaseURL: "https://app-crud-client-firebase.firebaseio.com",
+    projectId: "app-crud-client-firebase",
+    storageBucket: "",
+    messagingSenderId: "454161748519",
+    appId: "1:454161748519:web:cbc8318dd7ca17e7"
+  }
 };
